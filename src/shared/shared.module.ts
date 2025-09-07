56804c4dafee7102e3ae8313fe99d77f0702a7b6
phongnqse172516@gmail.com <phongnqse172516@gmail.com>
2025-09-07 13:30:38 +0700

    setup base source

---
+++ src/shared/shared.module.ts
@@ -0,0 +1,36 @@
+import { AccessTokenGuard } from '@/common/guards/access-token.guard'
+import { APIKeyGuard } from '@/common/guards/api-key.guard'
+import { AuthenticationGuard } from '@/common/guards/authentication.guard'
+import { SharedRoleRepository } from '@/shared/repositories/shared-role.repo'
+import { SharedUserRepository } from '@/shared/repositories/shared-user.repo'
+import { HashingService } from '@/shared/services/hashing.service'
+import { PrismaService } from '@/shared/services/prisma.service'
+import { TokenService } from '@/shared/services/token.service'
+import { Global, Module } from '@nestjs/common'
+import { APP_GUARD } from '@nestjs/core'
+import { JwtModule } from '@nestjs/jwt'
+
+const sharedServices = [
+  PrismaService,
+  HashingService,
+  TokenService,
+  // EmailService,
+  SharedUserRepository,
+  SharedRoleRepository
+]
+@Global()
+@Module({
+  imports: [JwtModule],
+  controllers: [],
+  providers: [
+    ...sharedServices,
+    AccessTokenGuard,
+    APIKeyGuard,
+    {
+      provide: APP_GUARD,
+      useClass: AuthenticationGuard
+    }
+  ],
+  exports: sharedServices
+})
+export class SharedModule {}
