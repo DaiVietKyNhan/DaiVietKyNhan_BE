9709c543ec21e05063ccf11335c91beb5973dcc1
Kumo <145751369+thuanhd2102@users.noreply.github.com>
2025-09-09 08:27:06 +0700

    eidt

--- src/3rdService/mail/mail.module.ts
+++ src/3rdService/mail/mail.module.ts
@@ -7,12 +7,12 @@ import { MailModule as MailerConfigModule } from './mailer/mail.module';
 
 @Module({
   imports: [
-    ConfigModule, // Để sử dụng ConfigService nếu cần
-    RedisModule, // Để sử dụng Redis cho OTP
-    MailerConfigModule, // Sử dụng MailerModule đã được cấu hình
+    ConfigModule, 
+    RedisModule, 
+    MailerConfigModule, 
   ],
   controllers: [MailController],
   providers: [MailService],
-  exports: [MailService], // Export MailService để các module khác có thể sử dụng
+  exports: [MailService], 
 })
 export class MailModule { }
--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -31,11 +31,13 @@ const configSchema = z.object({
   ADMIN_EMAIL: z.string(),
   PHONE_NUMBER: z.string(),
   OTP_EXPIRES_IN: z.string(),
+  //Redis
+  REDIS_URI: z.string(),
   // RESEND_API_KEY: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
   GOOGLE_REDIRECT_URI: z.string(),
-  GOOGLE_CLIENT_REDIRECT_URI: z.string()
+  GOOGLE_CLIENT_REDIRECT_URI: z.string(),
 })
 
 const configServer = configSchema.safeParse(process.env)
