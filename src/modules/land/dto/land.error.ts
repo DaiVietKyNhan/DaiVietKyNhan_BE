f616eab65bab6cd0ce25071c00b4120b44138eb7
phongnqse172516@gmail.com <phongnqse172516@gmail.com>
2025-10-20 22:03:39 +0700

    feat: add date

--- src/modules/land/dto/land.error.ts
+++ src/modules/land/dto/land.error.ts
@@ -1,3 +1,5 @@
 import { ConflictException } from '@nestjs/common'
 
 export const LandAlreadyExistsException = new ConflictException('Tên vùng này đã tồn tại')
+
+export const LandNotOpenedException = new ConflictException('Vùng này chưa đến ngày mở')
