2b82693c833dae82e3358571d42af915408fd4cc
Kumo <145751369+thuanhd2102@users.noreply.github.com>
2025-11-08 09:08:33 +0700

    done

--- src/modules/reward/user-reward-history.repo.ts
+++ src/modules/reward/user-reward-history.repo.ts
@@ -2,6 +2,8 @@ import { PaginationQueryType } from '@/shared/models/request.model'
 import { Injectable } from '@nestjs/common'
 import { Prisma } from '@prisma/client'
 import { PrismaService } from 'src/shared/services/prisma.service'
+import { parseQs } from '@/common/utils/qs-parser'
+import { USER_REWARD_HISTORY_FIELDS } from './entities/user-reward-history.entity'
 
 @Injectable()
 export class UserRewardHistoryRepo {
@@ -40,37 +42,82 @@ export class UserRewardHistoryRepo {
     }
 
     async list(pagination: PaginationQueryType) {
-        const { currentPage = 1, pageSize = 10 } = pagination
-        const skip = (currentPage - 1) * pageSize
+        const { where: parsedWhere, orderBy: parsedOrderBy } = parseQs(
+            pagination.qs,
+            USER_REWARD_HISTORY_FIELDS
+        )
+
+        // Initialize where clause if undefined
+        const whereClause: any = parsedWhere || {}
+
+        // Handle reward.code filter (relation field)
+        // Extract reward.code or code field and build nested filter
+        let rewardCodeFilter: any = {}
+        if (whereClause['reward.code']) {
+            // If explicit reward.code is provided
+            rewardCodeFilter = {
+                reward: {
+                    code: whereClause['reward.code']
+                }
+            }
+            delete whereClause['reward.code']
+        } else if (whereClause.code !== undefined) {
+            // If only 'code' is provided, filter by reward.code
+            // because in UserRewardHistory, code field is usually the same as reward.code
+            rewardCodeFilter = {
+                reward: {
+                    code: whereClause.code
+                }
+            }
+            delete whereClause.code
+        }
+
+        // Build where clause - merge all filters
+        // If there are other filters on reward relation, we need to merge them
+        const where: any = {
+            deletedAt: null,
+            ...whereClause
+        }
+
+        // Merge reward filters if rewardCodeFilter exists
+        if (Object.keys(rewardCodeFilter).length > 0) {
+            if (where.reward) {
+                // If reward filter already exists, merge them
+                where.reward = {
+                    ...where.reward,
+                    ...rewardCodeFilter.reward
+                }
+            } else {
+                // Otherwise, just add rewardCodeFilter
+                where.reward = rewardCodeFilter.reward
+            }
+        }
+
+        const skip = (pagination.currentPage - 1) * pagination.pageSize
+        const take = pagination.pageSize
 
         const [total, data] = await Promise.all([
             this.prisma.userRewardHistory.count({
-                where: {
-                    deletedAt: null
-                }
+                where
             }),
             this.prisma.userRewardHistory.findMany({
-                where: {
-                    deletedAt: null
-                },
+                where,
                 include: {
                     reward: true,
                     user: true
                 },
+                orderBy: parsedOrderBy || { createdAt: 'desc' },
                 skip,
-                take: pageSize,
-                orderBy: {
-                    createdAt: 'desc'
-                }
+                take
             })
         ])
 
         return {
             total,
             data,
-            currentPage,
-            pageSize,
-            totalPage: Math.ceil(total / pageSize)
+            currentPage: pagination.currentPage,
+            pageSize: pagination.pageSize,
+            totalPage: Math.ceil(total / pagination.pageSize)
         }
     }
 }
